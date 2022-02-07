2432eeb57cedb3b42dc3265410e2eacc0c5815dc
Jonathan Dobrowolski <19jd1@queensu.ca>
2022-02-06 21:15:27 -0500

    initial stuff

---
+++ not_32_gate.v
@@ -0,0 +1,17 @@
+`timescale 1ns/ 1ps
+
+module not_32_gate(
+			input wire [31:0] a_in,
+			output wire [31:0] b_out
+			);
+			
+			
+			genvar i;
+			generate
+					for(i = 0; i<32; i = i + 1)
+						begin: gen_loop
+							assign b_out[i] = !(a_in[i]);
+						end
+			endgenerate
+endmodule
+							
---
+++ or_32_gate.v
@@ -0,0 +1,18 @@
+`timescale 1ns/ 1ps
+
+module or_32_gate(
+			input wire [31:0] a_in,
+			input wire [31:0] b_in,
+			output wire [31:0] c_out
+			);
+			
+			
+			genvar i;
+			generate
+					for(i = 0; i<32; i = i + 1)
+						begin: gen_loop
+							assign c_out[i] = ((a_in[i])|(b_in[i]));
+						end
+			endgenerate
+endmodule
+							
