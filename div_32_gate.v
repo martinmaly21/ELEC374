61d310962e3f924dca699c36a997ddf615593e09
Martin Maly <martinmaly66@hotmail.com>
2022-02-10 12:45:01 -0500

    div_32_gate.v now functional

--- div_32_gate.v
+++ div_32_gate.v
@@ -1,35 +1,30 @@
 module div_32_gate(
-			input wire [31:0] dividendQ,
-			input wire [31:0] divisorM,
-			output reg [63:0] z_out
+			input wire [31:0] a_dividend,
+			input wire [31:0] b_divisor,
+			output reg [63:0] c_quotient_and_remainder
 			);	
-			reg [31:0] top  = [31:0] empty; //HERE
-			reg [31:0] bottom = a_in;
-			
-			reg [64:0] tempAnQ = 0;
-			reg [63:0] tempM = 0;
-			
-			//tempAnQ[64:32] = tempAnQ[64:32] + tempM;
-			
-			
-			/*
-			tempAnQ = {nothing, };
-			genvar i;
-			generate
-					for(i = 0; i<64; i = i + 1)
-						begin: gen_loop
-						
-							if (negative)
-							being
-							end
-							else 
-							//positive
-							begin
-							end
-							
-						end
-			endgenerate	
-			z_out <= tempAnQ[63:0];
-			*/
+	integer i; 
+	//sign extended
+	reg [32:0] dividend_se;
+	reg [64:0] quotient_and_remainder_se;
+
+	always @(a_dividend or b_divisor) begin
+		quotient_and_remainder_se[31:0] = b_divisor;
+		quotient_and_remainder_se[64:32] = 0;
+		dividend_se[31:0] = a_dividend;
+
+		for(i=0;i<32;i+=1) begin
+			dividend_se[32] = dividend_se[31];
+			quotient_and_remainder_se = quotient_and_remainder_se << 1;
+			quotient_and_remainder_se[64:32] = quotient_and_remainder_se[64:32] - a_dividend;
+
+			if (quotient_and_remainder_se[63] == 1) begin
+				quotient_and_remainder_se[0] = 0;
+				quotient_and_remainder_se[64:32] = quotient_and_remainder_se[64:32] + dividend_se;
+			end else if (quotient_and_remainder_se[63] == 0) begin
+				quotient_and_remainder_se[0] = 1;
+			end
+		end
+		c_quotient_and_remainder <= quotient_and_remainder_se[63:0];
+	end
 endmodule
-				//div, top is remainder, bottom is quotient
