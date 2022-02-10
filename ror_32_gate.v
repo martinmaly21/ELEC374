efd2d8bd85706e2d7ed33e301d3f8215b594e8b1
Martin Maly <martinmaly66@hotmail.com>
2022-02-10 15:45:12 -0500

    Remove modulos

--- ror_32_gate.v
+++ ror_32_gate.v
@@ -3,6 +3,6 @@ module ror_32_gate(
 	input wire [31:0] rotate_nums,
 	output reg [31:0] b_out
 );
-	assign b_out = ((a_in >> (rotate_nums % 32))|(a_in << (32 - (rotate_nums % 32))));
+	assign b_out = ((a_in >> rotate_nums)|(a_in << (32 - rotate_nums)));
 
 endmodule
